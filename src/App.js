60912df3f68c0a86cba768a530641c3aa02488e4
ozngrsc <ozangursucu@hotmail.com>
2022-04-12 14:47:49 +0300

    add logo

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React from "react";
+import Navbar from "./components/Navbar";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Yo</h1>
-    </div>
+    <>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" exact />
+        </Routes>
+      </Router>
+    </>
   );
 }
 
---
+++ src/components/Navbar.js
@@ -0,0 +1,24 @@
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+
+function Navbar() {
+  const [click, setClick] = useState(false);
+
+  const handleClick = () => setClick(!click);
+  return (
+    <>
+      <nav className="navbar">
+        <div className="navbar-container">
+          <Link to="/" className="navbar-logo">
+            TRVL <i className="fab fa-typo3" />
+          </Link>
+          <div className="menu-icon" onClick={handleClick}>
+            <i className={click ? "fas fa-times" : "fas fa-bars"} />
+          </div>
+        </div>
+      </nav>
+    </>
+  );
+}
+
+export default Navbar;
