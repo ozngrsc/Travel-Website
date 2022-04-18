52b221cadee8d87a011c5cc3d3cf931d56d7721b
ozngrsc <ozangursucu@hotmail.com>
2022-04-18 15:38:19 +0300

    play video

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import Navbar from "./components/Navbar";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
+import Home from "./components/pages/Home";
 
 function App() {
   return (
@@ -9,7 +10,7 @@ function App() {
       <Router>
         <Navbar />
         <Routes>
-          <Route path="/" exact />
+          <Route path="/" exact element={<Home />} />
         </Routes>
       </Router>
     </>
---
+++ src/components/pages/Home.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "../../App.css";
+import HeroSection from "../HeroSection";
+
+function Home() {
+  return (
+    <>
+      <HeroSection />
+    </>
+  );
+}
+
+export default Home;
