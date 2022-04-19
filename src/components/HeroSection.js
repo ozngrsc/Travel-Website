74af6631b8d6045d42b8924a3f2cef93e8f89b50
ozngrsc <ozangursucu@hotmail.com>
2022-04-19 15:03:22 +0300

    footer css

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -6,7 +6,7 @@ import "./HeroSection.css";
 function HeroSection() {
   return (
     <div className="hero-container">
-      <video src="/videos/video-2.mp4" autoPlay loop muted />
+      <video src="/videos/video-1.mp4" autoPlay loop muted />
       <h1>ADVANTURE AWAITS</h1>
       <p>What are you waiting for?</p>
       <div className="hero-btns">
