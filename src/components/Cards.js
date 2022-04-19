641444b5b164d76855f5235ffe18ecfae5fb769e
ozngrsc <ozangursucu@hotmail.com>
2022-04-19 11:33:18 +0300

    cards

---
+++ src/components/Cards.js
@@ -0,0 +1,25 @@
+import React from "react";
+import CardItem from "./CardItem";
+import "./Cards.css";
+
+function Cards() {
+  return (
+    <div className="cards">
+      <h1>Check out these EPIC Destinations!</h1>
+      <div className="cards__container">
+        <div className="cards__wrapper">
+          <ul className="cards__items">
+            <CardItem
+              src="images/img-9.jpg"
+              text="Explore the hidden waterfall deep inside the Amazon Jungle"
+              label="Adventure"
+              path="/services"
+            />
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Cards;
--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,11 +1,13 @@
 import React from "react";
 import "../../App.css";
+import Cards from "../Cards";
 import HeroSection from "../HeroSection";
 
 function Home() {
   return (
     <>
       <HeroSection />
+      <Cards />
     </>
   );
 }
