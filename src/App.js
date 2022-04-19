7d75f4de0a00924ff6ac9fd4d6122b34771f7ce3
ozngrsc <ozangursucu@hotmail.com>
2022-04-19 11:51:32 +0300

    link

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import Navbar from "./components/Navbar";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
 import Home from "./components/pages/Home";
+import Services from "./components/pages/Services";
+import Products from "./components/pages/Products";
+import SignUp from "./components/pages/SignUp";
 
 function App() {
   return (
@@ -11,6 +14,9 @@ function App() {
         <Navbar />
         <Routes>
           <Route path="/" exact element={<Home />} />
+          <Route path="/services" element={<Services />} />
+          <Route path="/products" element={<Products />} />
+          <Route path="/sign-up" element={<SignUp />} />
         </Routes>
       </Router>
     </>
--- src/components/Cards.js
+++ src/components/Cards.js
@@ -15,6 +15,32 @@ function Cards() {
               label="Adventure"
               path="/services"
             />
+            <CardItem
+              src="images/img-2.jpg"
+              text="Travel through the Island of Bali in a Private Cruise"
+              label="Luxury"
+              path="/services"
+            />
+          </ul>
+          <ul className="cards__items">
+            <CardItem
+              src="images/img-3.jpg"
+              text="Set Sail in the Atlantic Ocean visiting Uncharted Waters"
+              label="Mystery"
+              path="/services"
+            />
+            <CardItem
+              src="images/img-4.jpg"
+              text="Experience Football on Top of the Himilayan Mountains"
+              label="Adventure"
+              path="/products"
+            />
+            <CardItem
+              src="images/img-8.jpg"
+              text="Ride through the Sahara Desert on a guided camel tour"
+              label="Adrenaline"
+              path="/sign-up"
+            />
           </ul>
         </div>
       </div>
